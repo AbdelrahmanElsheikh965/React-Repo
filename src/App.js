ca390f8d1f011c81ed1be689cc9d35393b6415fe
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-03-19 17:47:59 +0200

    added products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import Card from "./components/card";
+import { useState, useEffect } from "react";
+import Products from "./components/Products";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Card, Col, Button } from "react-bootstrap";
 
 function App() {
+
   return (
-    <>
-      <p>test</p>
-    </>
+    
+    <Products />  
   );
 }
 
---
+++ src/components/card/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import './index.css'
+
+export default function Card({product}) {
+
+
+  return (
+    <div className="col-md-4">
+    <div className="work-box">
+      <a href="./assets/img/work-1.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox">
+        <div className="work-img" style={{'width': '250px', 'height': '120px'}}>
+          <img src={product.thumbnail}  className="img-fluid" />
+        </div>
+      </a>
+      <div className="work-content">
+        <div className="row">
+          <div className="col-sm-8">
+            <h2 className="w-title">{product.title}</h2>
+            <div className="w-more">
+              <span className="w-ctegory">{product.category}</span> / <span className="w-date">Category</span>
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <div className="w-like">
+              <a href="portfolio-details.html"> <span className="bi bi-plus-circle"></span></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+
+}
